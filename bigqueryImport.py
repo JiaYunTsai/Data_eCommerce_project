e8a4ec227a6db6b3dacb3d425c03c3fccab3ba78
DESKTOP-I0HET24\developergxc <developergxc@gmail.com>
2023-05-17 10:36:55 +0800

    上傳從 Cloud Storage 匯入資料至 BigQuery 的 Python 檔案

---
+++ bigqueryImport.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import dask.dataframe as dd
+import gcsfs
+from pandas_gbq import to_gbq
+from google.oauth2.service_account import Credentials
+
+import os
+
+#/* GCS Pre-Define */
+projectName= "My First Project"
+projectDataset = "91app_dataset"
+datasetPath = "Behaviordata"
+
+#/* GCS API */
+gcsAPI = "./lustrous-setup-386509_GCS_API_Key.json"
+gbqAPI = "./lustrous-setup-386509_GBQ_API_Key.json"
+
+# /* 連接至 GCS */
+fs = gcsfs.GCSFileSystem(project=projectName,token=gcsAPI)
+
+# /* 取得 GCS 中的特定檔案 */
+fileList = fs.ls(projectDataset+"/"+datasetPath)
+_newfileList = ["gs://" + x for x in fileList]
+print(_newfileList)
+
+# /* dask */
+# /* 第一個為 gs://91app_dataset/Behaviordata/，所以跳過從 1 開始 */
+#/* datatype 轉換錯誤，靜態設置  */
+dtypes={'DeviceId': 'object',
+       'EventTime': 'float64',
+       'HitTime': 'float64',
+       'Version': 'object',
+       'CategoryId': 'object',
+       'RegisterTunnel': 'object',
+       'SearchTerm': 'object',
+       'TradesGroupCode': 'object'}
+ddf = dd.read_csv(_newfileList[1], dtype=dtypes,storage_options={"token": gcsAPI})
+
+# /* dask to  pandas */
+df_pd = ddf.compute()
+credentials = Credentials.from_service_account_file(gbqAPI)
+tableName = "BehaviorData"
+to_gbq(df_pd, tableName+".BehaviorData", project_id="lustrous-setup-386509", credentials=credentials, if_exists='replace')
