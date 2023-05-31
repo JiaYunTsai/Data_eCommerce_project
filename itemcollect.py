07fa0ef2499a70f26b9f8723c53103f62c2696e0
Aston <aston@cengyangtengdeMacBook-Air.local>
2023-05-31 16:22:35 +0800

    添加Xin

--- itemcollect.py
+++ itemcollect.py
@@ -18,6 +18,7 @@ class EachMemberItem:
         }
         self.member.loc[:, "score"] = self.member["Behavior"].map(self.score).copy()
         self.member_item_dict = {}
+        self.member_buy_dict = {}
 
     def item_dict(self):
         self.member["score"].fillna(0, inplace=True)
@@ -28,13 +29,21 @@ class EachMemberItem:
                 self.member["SalePageId"] == i, "score"
             ].sum()
         self.member_item_dict = member_item_dict
-        # return member_item_dict
+
+    def buy_dict(self):
+        purchase = self.member.loc[
+            self.member["Behavior"] == "purchase", "SalePageId"
+        ].to_list()
+        member_buy_dict = {}
+        for i in purchase:
+            member_buy_dict[i] = 1
+        self.member_buy_dict = member_buy_dict
 
 
 class UserItem:
     def __init__(self, data):
         self.behaviordata = pd.read_excel(data)
-        # 因應推薦序桶排序時需要使用數字
+        # 因應推薦系統排序時需要使用數字
         self.behaviordata["ShopMemberId"] = pd.factorize(
             self.behaviordata["ShopMemberId"]
         )[0]
@@ -43,106 +52,148 @@ class UserItem:
         self.allitem = set(self.behaviordata["SalePageId"].to_list())
         self.nummatrix = None
         self.last_matrix = None
+        self.buynummatrix = None
 
     def user_item_dict(self):
         allmember = self.allmember
         behaviordata = self.behaviordata
         memberitem_matrix = {}
+        memberbuy_matrix = {}
         for i in allmember:
             memberdata = behaviordata.loc[behaviordata["ShopMemberId"] == i]
             memberitem = EachMemberItem(memberdata)
             memberitem.item_dict()
+            memberitem.buy_dict()
             memberitem_matrix[i] = memberitem.member_item_dict
+            memberbuy_matrix[i] = memberitem.member_buy_dict
         self.memberitem_matrix = memberitem_matrix
+        self.memberbuy_matrix = memberbuy_matrix
 
     def martix(self):
         allitem = list(self.allitem)
         allmember = list(self.allmember)
         memberitem_matrix = self.memberitem_matrix
+        buy_matrix = self.memberbuy_matrix
         last_matrix = pd.DataFrame(columns=allitem, index=allmember)
+        buy_yes_matrix = pd.DataFrame(columns=allitem, index=allmember)
         for i in allmember:
             for j in allitem:
                 if memberitem_matrix[i].get(j) == None:
                     last_matrix.loc[i, j] = 0
                 else:
                     last_matrix.loc[i, j] = memberitem_matrix[i].get(j)
+                if buy_matrix[i].get(j) == None:
+                    buy_yes_matrix.loc[i, j] = 0
+                else:
+                    buy_yes_matrix.loc[i, j] = buy_matrix[i].get(j)
+
         last_matrix.index.name = "userId"
         last_matrix.to_excel("last_martix.xlsx")
+        buy_yes_matrix.index.name = "userId"
+        buy_yes_matrix.to_excel("buy_yes_martix.xlsx")
         self.nummatrix = last_matrix.values
         self.last_matrix = last_matrix
+        self.buynummatrix = buy_yes_matrix.values
 
 
-class recommend_system:
-    def recommend_similarity(user_id, user_similarity, user_item_matrix):
-        unrated_products = user_item_matrix.loc[user_id, :].isna()
-        unrated_products = unrated_products.index.values
-        k = 3  # 找k位最相似的
+class Recommend_system:
+    def __init__(self):
+        self.user_similarity = None
+        self.item_sim_matrix = None
+
+    def martix_to_similarity(self, nummatrix):
+        self.user_similarity = cosine_similarity(nummatrix)
+        self.item_sim_matrix = np.dot(nummatrix.T, nummatrix)
+
+    def recommend_similarity(
+        self, user_id, user_similarity, user_item_matrix, k
+    ):  # 找k個最相似的
+        unrated_products = user_item_matrix.loc[user_id, :].isna()  # 未瀏覽過
+        unrated_products = unrated_products.index.values  # 商品ID
         similar_users = user_similarity[user_id].argsort()[-k:][::-1]
         similar_user_ratings = user_item_matrix.loc[similar_users, unrated_products]
         recommended_ratings = similar_user_ratings.mean(axis=0)
         recommended_ratings = np.array(recommended_ratings).argsort()[::-1]
-        return unrated_products[recommended_ratings[:k]]
-
-    def recommend_content(user_id, user_item_matrix):
-        unrated_products_index = pd.Index(pd.isna(user_item_matrix[user_id]))
-
-        # Predict the ratings for the unrated products.
-        predicted_ratings = user_item_matrix.dot(
-            svd.inverse_transform(user_item_matrix[user_id])
+        return (
+            similar_users.tolist(),
+            unrated_products[recommended_ratings[:k]].tolist(),
         )
 
-        # Sort the predicted ratings by descending order.
-        predicted_ratings = np.array(predicted_ratings).argsort()[::-1]
 
-        # Return the top-k recommended products.
-        return user_item_matrix.columns[unrated_products_index][predicted_ratings[:5]]
+class ProductName:
+    def __init__(self, dataset):
+        self.dataset = pd.read_csv(dataset)
+        self.salepage_dict = self.create_salepage_dict()
 
-    def generate_recommendations(user_id, top_n):
-        user_item_matrix = usermatrix.last_matrix
-        user_ratings = user_item_matrix.loc[user_id]
-        user_predictions = item_sim_matrix.dot(user_ratings)
-        recommended_indices = user_predictions.argsort()[-top_n:][::-1]
-        recommended_products = user_item_matrix.columns[recommended_indices]
+    def create_salepage_dict(self):
+        salepage_dict = {}
+        for index, row in self.dataset.iterrows():
+            salepage_id = row["SalePage_Id"]
+            salepage_title = row["SalePage_Title"]
+            salepage_dict[salepage_id] = salepage_title
+
+        return salepage_dict
+
+    def get_salepage_title(self, salepage_id):
+        return self.salepage_dict.get(salepage_id, "Title not found")
+
+
+class TrainTestModel:
+    def __init__(self, user_features, train_data, test_data):
+        self.user_features = user_features
+        self.train_data = train_data
+        self.test_data = test_data
+
+    def train(self, target_user):
+        # 找到具有相同特徵的人（Group AA）
+        group_aa = self.user_features.loc[self.user_features == target_user]
+
+        # 從Group AA中獲取喜歡的產品A, B, C
+        liked_products = self.train_data.loc[
+            group_aa, ["Product_A", "Product_B", "Product_C"]
+        ]
+
+        # 推薦給目標用戶
+        recommended_products = liked_products.mean(axis=0)
         return recommended_products
 
+    def test(self, target_user):
+        # 找到具有相同特徵的人（Group AA）
+        group_aa = self.user_features.loc[self.user_features == target_user]
 
-##寫一個 user_item_matrix
-"""
-usermatrix.nummatrix 評分矩陣
+        # 從Group AA中獲取這季度喜歡的產品E, F, G
+        current_likes = self.test_data.loc[
+            group_aa, ["Product_E", "Product_F", "Product_G"]
+        ]
 
-usermatrix.last_matrix 評分的DF
+        # 找到具有相同特徵的人（Group BB）
+        group_bb = self.user_features.loc[self.user_features == target_user]
 
-"""
-#
+        # 從Group BB中獲取這季度喜歡的產品H, J
+        new_likes = self.test_data.loc[group_bb, ["Product_H", "Product_J"]]
+
+        # 比較新喜歡的產品H, J與目前喜歡的產品E, F, G
+        comparison = new_likes.compare(current_likes)
+        return comparison
 
 
 if __name__ == "__main__":
-    usermatrix = UserItem(
-        "/Users/aston/Downloads/91APP_DataSet_2023/BehaviorData/Data_eCommerce_project/testid_2_num.xlsx"
-    )
+    usermatrix = UserItem("testid_31.xlsx")
     usermatrix.user_item_dict()
     usermatrix.martix()
-    # print(usermatrix.last_matrix)
-    # print(usermatrix.nummatrix)
 
-    user_similarity = cosine_similarity(usermatrix.nummatrix)
-    item_sim_matrix = np.dot(usermatrix.nummatrix.T, usermatrix.nummatrix)
-
-    recommend_result = recommend_system.recommend_similarity(
-        usermatrix.last_matrix.index[2], user_similarity, usermatrix.last_matrix
+    rs = Recommend_system()
+    rs.martix_to_similarity(usermatrix.nummatrix)
+    recommendations = rs.recommend_similarity(
+        usermatrix.last_matrix.index[0], rs.user_similarity, usermatrix.last_matrix, 5
     )
-    # print(recommend_result)
-    usermatrix.last_matrix.columns = usermatrix.last_matrix.columns.astype(str)
-    # print(type(usermatrix.last_matrix.columns[0]))
-    # print(type(usermatrix.last_matrix))
-    svd = TruncatedSVD(n_components=100)
-    user_item_matrix_svd = svd.fit_transform(usermatrix.last_matrix)
-    recommended_products = user_item_matrix_svd[
-        usermatrix.last_matrix.index[2]
-    ].argsort()[::-1][:4]
-    user_id = usermatrix.last_matrix.index[2]
-    recommendations = recommend_system.generate_recommendations(user_id, 3)
-    # print(pd.DataFrame(user_item_matrix_svd))
-    print(recommended_products)
-    # print(recommendations)
-    print(f"Top 3 Recommendations for User {user_id}: {recommendations}")
+
+    product_name = ProductName("SalePageData.csv")
+    for salepage_id, i in zip(
+        recommendations[1], range(1, len(recommendations[1]) + 1)
+    ):
+        salepage_title = product_name.get_salepage_title(salepage_id)
+        print(f"推薦第{i}名為：{salepage_title} ")
+    # print(rs.user_similarity)
+    # print(recommendations[0])
+    print(usermatrix.last_matrix.loc[[30, 29]])
